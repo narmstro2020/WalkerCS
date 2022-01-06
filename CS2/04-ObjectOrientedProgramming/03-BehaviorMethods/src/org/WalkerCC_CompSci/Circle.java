2a5213517827239d12cd95d7fcec2bd1436bd14e
narmstro2020 <narmstro@warren.k12.in.us>
2022-01-06 12:19:01 -0500

    Updated Unit 4, Lesson 3:  Behavior Methods

---
+++ CS2/04-ObjectOrientedProgramming/03-BehaviorMethods/src/org/WalkerCC_CompSci/Circle.java
@@ -0,0 +1,54 @@
+package org.WalkerCC_CompSci;
+
+public class Circle {
+    //Field (Attributes) marked private because we should access by Getters and Setters
+    private double radius;
+    private String name;
+
+    //Constructor:  public "name of the Class"(input parameters)
+    public Circle(String name, double radius){
+        this.name = name;
+        this.radius = radius;
+    }
+
+    //getter Method that gets the radius:  return type double.  public of course
+    public double getRadius(){
+        return this.radius;
+    }
+
+    //setter Method that sets the radius:  return type void.  public of course
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    //getter Method the gets the name:  return type String.  public of course.
+    //Never allowed to be set
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString(){
+        String outputString = "";
+
+        outputString += this.name;
+        outputString += " ";
+        outputString += "has a radius of";
+        outputString += " ";
+        outputString += this.radius;
+
+        return outputString;
+    }
+
+    public double Diameter(){
+        return 2 * this.radius;
+    }
+
+    public double Circumference(){
+        return 2 * Math.PI * this.radius;
+    }
+
+    public double Area(){
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+}
--- CS2/04-ObjectOrientedProgramming/03-BehaviorMethods/src/org/WalkerCC_CompSci/Main.java
+++ CS2/04-ObjectOrientedProgramming/03-BehaviorMethods/src/org/WalkerCC_CompSci/Main.java
@@ -3,6 +3,15 @@ package org.WalkerCC_CompSci;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       Circle circle1 = new Circle("Circle1", 5);
+
+       System.out.println(circle1);
+
+       circle1.setRadius(10);
+       System.out.println(circle1);
+
+        System.out.println(circle1.getName() + " has a diameter of " + circle1.Diameter());
+        System.out.println(circle1.getName() + " has a circumference of " + circle1.Circumference());
+        System.out.println(circle1.getName() + " has a area of " + circle1.Area());
     }
 }
